01ecdadcee0c591827226f3bdf77fc2d704c94b9
Gregory Berman <gregory.berman@gmail.com>
2016-12-31 00:15:03 -0500

    created a basic programed board for tic tac toe with the ability to click the buttons / boxes. also cleaned the using statements for every file

--- TileGames/TicTacToeDesktopUI/Form1.Designer.cs
+++ TileGames/TicTacToeDesktopUI/Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(315, 315);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 315);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        public System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
--- TileGames/TicTacToeDesktopUI/Form1.cs
+++ TileGames/TicTacToeDesktopUI/Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace TicTacToeDesktopUI
@@ -15,6 +8,36 @@ namespace TicTacToeDesktopUI
         public Form1()
         {
             InitializeComponent();
+            flowLayoutPanel1.BackColor = Color.DarkGray;
+            for (int i = 0; i < 9; i++)
+            {
+                flowLayoutPanel1.Controls.Add(CreateButton(i, ""));
+            }
+        }
+        static Button CreateButton(int index, string defaultValue)
+        {
+            var button = new Button();
+            button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button.Location = new System.Drawing.Point(15, 15);
+            button.Margin = new System.Windows.Forms.Padding(15);
+            button.Name = index.ToString();
+            button.Size = new System.Drawing.Size(75, 75);
+            button.TabIndex = 0;
+            button.TabStop = false;
+            button.Text = defaultValue;
+            button.UseVisualStyleBackColor = true;
+            button.BackColor = Color.WhiteSmoke;
+            button.Click += ButtonClicked;
+            return button;
+        }
+
+        private static void ButtonClicked(object sender, System.EventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null)
+                return;
+
+            button.Text = "X";
         }
     }
 }
